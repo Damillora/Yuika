e7ea38a451d5187f9a3ddc94a4785d3fc03298bb
Damillora <developer@damillora.com>
2020-11-11 11:54:56 +0700

    Yuika v2.0

--- assets/js/nav-collapse.js
+++ assets/js/nav-collapse.js
@@ -1,49 +1,6 @@
-    $(document).ready(function () {
-        
-        var nav = document.querySelector('.index-navbar');
-        var feed = document.querySelector('.content');
-
-        var lastScrollY = window.scrollY;
-        var lastWindowHeight = window.innerHeight;
-        var lastDocumentHeight = $(document).height();
-        var ticking = false;
-
-        function onScroll() {
-            lastScrollY = window.scrollY;
-            requestTick();
-        }
-
-        function onResize() {
-            lastWindowHeight = window.innerHeight;
-            lastDocumentHeight = $(document).height();
-            requestTick();
-        }
-
-        function requestTick() {
-            if (!ticking) {
-                requestAnimationFrame(update);
-            }
-            ticking = true;
-        }
-
-        function update() {
-            var trigger = feed.getBoundingClientRect().top + window.scrollY;
-            var progressMax = lastDocumentHeight - lastWindowHeight;
-
-            // show/hide nav
-            if (lastScrollY >= nav.getBoundingClientRect().bottom) {
-                nav.classList.add('nav-fixed');
-            } else {
-                nav.classList.remove('nav-fixed');
-            }
-
-            ticking = false;
-        }
-
-        window.addEventListener('scroll', onScroll, { passive: true });
-        window.addEventListener('resize', onResize, false);
-
-        update();
-
-    });
-
+$("#menushow").click(function(){
+    $(".index-navbar").toggleClass('enabled');
+})
+$(".nav li").click(function(){
+    $(".index-navbar").toggleClass('enabled');
+})
