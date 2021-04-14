c7187ebcc4b145b5b2a8c720ddc8a22e712fc955
Damillora <damillora@damillora.com>
2021-04-14 22:19:52 +0700

    feat(theme): persist dark mode

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,14 +1,27 @@
+function setDarkMode() {
+    if (!window.localStorage.getItem('theme') && window.document.body.getAttribute("data-theme")){
+        window.localStorage.setItem('theme', window.document.body.getAttribute("data-theme"));
+    }
+    if (window.document.body.getAttribute("data-theme") == "dark") {
+        $("#darkMode i").text("brightness_high");
+        $("#darkMode p").text("Light Mode");
+    } else {
+        $("#darkMode i").text("brightness_low");
+        $("#darkMode p").text("Dark Mode");
+    }
+}
+if (window.localStorage.getItem('theme')) {
+    window.document.body.setAttribute("data-theme", window.localStorage.getItem("theme"));
+}
 
 $(document).ready(function () {
+    setDarkMode();
     $("#darkMode").click(function () {
         if (window.document.body.getAttribute("data-theme") != "dark") {
             window.document.body.setAttribute("data-theme", "dark");
-            $("#darkMode i").text("brightness_high");
-            $("#darkMode p").text("Light Mode");
         } else {
             window.document.body.setAttribute("data-theme", "light");
-            $("#darkMode i").text("brightness_low");
-            $("#darkMode p").text("Dark Mode");
         }
+        setDarkMode();
     })
 });
